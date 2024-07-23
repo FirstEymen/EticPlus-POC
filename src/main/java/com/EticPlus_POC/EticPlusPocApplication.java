a1d52fb5409c08f0771f38ec52502d5420a5a603
eymenbilir <a.eymenbilir@gmail.com>
2024-07-23 16:20:19 +0300

    ilk commit

---
+++ src/main/java/com/EticPlus_POC/EticPlusPocApplication.java
@@ -0,0 +1,14 @@
+package com.EticPlus_POC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EticPlusPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EticPlusPocApplication.class, args);
+	}
+
+
+}
