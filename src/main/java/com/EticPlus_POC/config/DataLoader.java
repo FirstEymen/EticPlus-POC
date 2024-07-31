9da3cefb1cb5a82cde46c9ced52f55939a801ae2
eymenbilir <a.eymenbilir@gmail.com>
2024-07-31 14:45:45 +0300

    category update2

--- src/main/java/com/EticPlus_POC/config/DataLoader.java
+++ src/main/java/com/EticPlus_POC/config/DataLoader.java
@@ -12,18 +12,20 @@ public class DataLoader {
     @Bean
     CommandLineRunner loadData(StoreCategoryRepository repository) {
         return args -> {
-            repository.save(new StoreCategory(null, "General Store"));
-            repository.save(new StoreCategory(null, "Brand Store"));
-            repository.save(new StoreCategory(null, "Boutique Store"));
-            repository.save(new StoreCategory(null, "Crafts and Hobby Store"));
-            repository.save(new StoreCategory(null, "Food and Beverage Store"));
-            repository.save(new StoreCategory(null, "Cosmetics and Personal Care Store"));
-            repository.save(new StoreCategory(null, "Electronics Store"));
-            repository.save(new StoreCategory(null, "Home and Garden Store"));
-            repository.save(new StoreCategory(null, "Sports and Outdoor Store"));
-            repository.save(new StoreCategory(null, "Books and Music Store"));
-            repository.save(new StoreCategory(null, "Kids and Baby Store"));
-            repository.save(new StoreCategory(null, "Category-Specific Store"));
+            if (repository.count() == 0) {
+                repository.save(new StoreCategory(null, "General Store"));
+                repository.save(new StoreCategory(null, "Brand Store"));
+                repository.save(new StoreCategory(null, "Boutique Store"));
+                repository.save(new StoreCategory(null, "Crafts and Hobby Store"));
+                repository.save(new StoreCategory(null, "Food and Beverage Store"));
+                repository.save(new StoreCategory(null, "Cosmetics and Personal Care Store"));
+                repository.save(new StoreCategory(null, "Electronics Store"));
+                repository.save(new StoreCategory(null, "Home and Garden Store"));
+                repository.save(new StoreCategory(null, "Sports and Outdoor Store"));
+                repository.save(new StoreCategory(null, "Books and Music Store"));
+                repository.save(new StoreCategory(null, "Kids and Baby Store"));
+                repository.save(new StoreCategory(null, "Category-Specific Store"));
+            }
         };
     }
 }
